2a6d753b3c5c49fdebd3536963c767edcecd14bc
Achraf Mansari <contact@achrafmansari.com>
2021-07-12 20:34:53 +0100

    Generate random usernames and passwords

--- passwork_username_generator.py
+++ passwork_username_generator.py
@@ -6,12 +6,13 @@ username_length = 10
 password_length = 20
 
 while True:
-    # Randomness generator
-    un = string.ascii_letters + string.digits
-    random_un = "".join(random.choice(un) for i in range(username_length))
-    pw = string.ascii_letters + string.hexdigits + string.punctuation
-    random_pw = "".join(random.choice(pw) for x in range(password_length))
+    for t in range(8):
+        # Randomness generator
+        un = string.ascii_letters + string.digits
+        random_un = "".join(random.choice(string.ascii_letters) for i in range(10))
+        pw = string.ascii_letters + string.hexdigits + string.punctuation
+        random_pw = "".join(random.choice(pw) for x in range(password_length))
 
-    # Print a random username and password
-    print("Username : ", random_un, " | ", "Password : ", random_pw)
-    input('-' * 60)  # Press ENTER to regenerate
+        # Print a random username and password
+        print("Username : {}  |  Password : {}\n{}".format(random_un, random_pw, "-" * 60))
+    input()  # Press ENTER to regenerate
